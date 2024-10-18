1d9e0ecd64634d0ef92efe59b332b5fb231b043f
Edward O'Callaghan <edward@antitrust.cc>
2024-10-18 11:39:37 +1100

    sixpack: Factor out example JS animation
    Signed-off-by: Edward O'Callaghan <edward@antitrust.cc>

---
+++ js/sixpack.js
@@ -0,0 +1,32 @@
+			var attitude = $.flightIndicator('#attitude', 'attitude', {roll:50, pitch:-20, size:200, showBox : true});
+			var heading = $.flightIndicator('#heading', 'heading', {heading:150, showBox:true});
+			var variometer = $.flightIndicator('#variometer', 'variometer', {vario:-5, showBox:true});
+			var airspeed = $.flightIndicator('#airspeed', 'airspeed', {showBox: false});
+			var altimeter = $.flightIndicator('#altimeter', 'altimeter');
+			var turn_coordinator = $.flightIndicator('#turn_coordinator', 'turn_coordinator', {turn:0});
+
+			// Update at 20Hz
+			var increment = 0;
+			setInterval(function() {
+			    // Airspeed update
+			    airspeed.setAirSpeed(80+80*Math.sin(increment/10));
+
+			    // Attitude update
+			    attitude.setRoll(30*Math.sin(increment/10));
+			    attitude.setPitch(50*Math.sin(increment/20));
+
+			    // Altimeter update
+			    altimeter.setAltitude(10*increment);
+			    altimeter.setPressure(1000+3*Math.sin(increment/50));
+			    increment++;
+
+			    // TC update
+			    turn_coordinator.setTurn(30*Math.sin(increment/10));
+
+			    // Heading update
+			    heading.setHeading(increment);
+
+			    // Vario update
+			    variometer.setVario(2*Math.sin(increment/10));
+
+			}, 50);
