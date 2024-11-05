76738a18a5dec6f41accb0940c1a352a783c74ff
Edward O'Callaghan <edward@antitrust.cc>
2024-11-05 21:50:04 +1100

    js/kx155.js: Hook kx155 on-load into audio tag
    Signed-off-by: Edward O'Callaghan <edward@antitrust.cc>

--- js/kx155.js
+++ js/kx155.js
@@ -1,25 +1,47 @@
-$("#kx155 .vol-off").on("click", function() {
-	console.log("Volume");
-});
-$("#kx155 .pull-25k").on("click", function() {
-	console.log("Pull 25K");
-});
-$("#kx155 .nav-knob").on("click", function() {
-	console.log("Nav Knob");
-});
-$("#kx155 .pull-ident").on("click", function() {
-	console.log("Pull Ident");
-});
+const audio = document.querySelector("audio");
+audio.removeAttribute('controls');
 
-$("#kx155 .toggle-comm-freq").on("click", function() {
-	console.log("Toggling Comm Freq");
-});
-$("#kx155 .toggle-nav-freq").on("click", function() {
-	console.log("Toggling Nav Freq");
-});
+const audio_title = document.getElementById("audio-title");
 
-$.getJSON('https://bac-ctaf-srv.bathurstaeroclub.com.au/status-json.xsl', function(data) {
-	console.log(data);
-	document.getElementById("audio-title").innerHTML = data.icestats.source.title;
-//	$("#kx155.comm-display").innerHTML = data.icestats.source.title;
+const svgObj = $("#kx155");
+
+svgObj.on("load", () => {
+	console.log("kx155 svg loaded.");
+	const svg = svgObj[0].contentDocument;
+	//console.log( svg );
+
+	let isPlaying = false;
+
+	$("svg .vol-off", svg).on("click", () => {
+		console.log("Volume");
+		if (isPlaying) {
+			audio.pause();
+		} else {
+			audio.play();
+		}
+		isPlaying = !isPlaying;
+	});
+
+	$(".pull-25k", svg).on("click", () => {
+		console.log("Pull 25K");
+	});
+	$(".nav-knob", svg).on("click", () => {
+		console.log("Nav Knob");
+	});
+	$(".pull-ident", svg).on("click", () => {
+		console.log("Pull Ident");
+	});
+
+	$(".toggle-comm-freq", svg).on("click", () => {
+		console.log("Toggling Comm Freq");
+	});
+	$(".toggle-nav-freq", svg).on("click", () => {
+		console.log("Toggling Nav Freq");
+	});
+
+	$.getJSON('https://bac-ctaf-srv.bathurstaeroclub.com.au/status-json.xsl', (data) => {
+		console.log(data);
+		audio_title.innerHTML = data.icestats.source.title;
+		$(".comm-display").innerHTML = data.icestats.source.title;
+	});
 });
